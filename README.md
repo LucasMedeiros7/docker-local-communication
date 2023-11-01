89865c8a48c237bb26fb83e83b6dccd64c6ccfb6
LucasMedeiros7 <lucaasmedeiros7@gmail.com>
2023-11-01 18:43:16 -0300

    improve on script run.sh

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 ### Communication Between Docker Services and Local Applications
+
+Requirements:
+
+node.js must be installed.
--- run.sh
+++ run.sh
@@ -3,5 +3,7 @@ sudo apt install net-tools
 
 my_ip=$(ifconfig | grep -A1 "wlx001a3f2b6b0b" | awk '/inet / {print $2}')
 
+node ./apps/hello-app.js
+
 docker build -t node-networking .
 docker run -e DATABASE_HOST="$my_ip" node-networking:latest
