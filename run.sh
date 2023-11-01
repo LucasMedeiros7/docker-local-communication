5d68ad31c849a59b484d618995d7851744e9515e
LucasMedeiros7 <lucaasmedeiros7@gmail.com>
2023-11-01 18:41:19 -0300

    create automation get ip address and run container

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt install net-tools
+
+my_ip=$(ifconfig | grep -A1 "wlx001a3f2b6b0b" | awk '/inet / {print $2}')
+
+docker build -t node-networking .
+docker run -e DATABASE_HOST="$my_ip" node-networking:latest
